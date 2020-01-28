fd2618726ca7bbe0d95768ca26ca229f7b6a1568
Freda Chen <fredachen98@gmail.com>
2020-01-27 20:26:34 -0500

    Booking update

--- src/BookingScreen.js
+++ src/BookingScreen.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import './BookingWindow.css';
 
 export default function BookingScreen() {
     return(
-
-        <div>Random text to be filled</div>
+<React.Fragment>
+<script src="%PUBLIC_URL%/BookingScript.js"></script>
+<body onload="createTable()">
+    <div id="bookingDetail">
+        <div class="bookingForm">
+            <p>
+                <li> Name: </li>
+                <input type="text" id="username" />
+                <li> Address: </li>
+                <input type="text" id="address" />
+            </p>
+            <div id="roomList"></div>
+            <p>
+                <input type="button" id="addRow" value="Add Row" onclick="addRow()" />
+                <input type="button" id="submit" value="Submit Data"/>
+                <li> Date: </li>
+                <input type="datetime" id="date" />
+            </p>
+        </div>
+    </div>
+</body>
+</React.Fragment>
     );
 }
