65969f73124925990ab9ae3cea06da0b8d0e02b5
Andrew Hoyt <andrew@chillhammer.com>
2020-02-03 17:00:58 -0500

    Base Room Entry HTML and CSS
    Little pocket showing drop down, check box, and text for room name

--- src/BookingScreen.js
+++ src/BookingScreen.js
@@ -1,28 +1,57 @@
 import React from 'react';
 import './BookingWindow.css';
 
-export default function BookingScreen() {
-    return(
-<React.Fragment>
-<script src="%PUBLIC_URL%/BookingScript.js"></script>
-<div id="bookingDetail">
-    <div class="bookingForm">
-        <p>
-            <label>Name</label><br/>
-            <input type="text" id="username" class="text"/>
-            <label>Address</label><br/>
-            <input type="text" id="address" class="text"/>
-        </p>
-        <div id="roomList"></div>
-        <p>
-            <input type="button" id="addRow" value="Add Row" onclick="addRow()" />
-            <input type="button" id="submit" value="Submit Data"/>
-            <br></br>
-            <label>Date</label><br/>
-            <input type="datetime" id="date" class="text" />
-        </p>
-    </div>
-</div>
-</React.Fragment>
-    );
+
+class BookingScreen extends React.Component {
+
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+      handleSubmit(event) {
+        console.log("Submitting");
+        event.preventDefault();
+        const data = new FormData(event.target);
+        
+        fetch('/api/form-submit-url', {
+          method: 'POST',
+          body: data,
+        });
+      }
+
+    render() {
+        return (
+        <div id="bookingDetail">
+            <div class="bookingForm">
+                <form onSubmit={this.handleSubmit}>
+                    <label>Name</label><br/>
+                    <input type="text" id="username" name="name" class="text"/>
+                    <label>Address</label><br/>
+                    <input type="text" id="address" name="Address" class="text"/>
+                    <label>Date</label><br/>
+                    <input type="datetime" id="date" name="Date" class="text" />
+                
+                    <div id="roomList">
+
+                        <div class="roomEntry">
+                            <label>Living Room</label>
+                            <select>
+                                <option value = "1">1</option>
+                                <option value = "2">2</option>
+                                <option value = "3">3</option>
+                                <option value = "4">4</option>
+                            </select>
+                            <input type="checkbox"></input>
+                        </div>
+                    </div>
+                    <input type="button" id="addRow" value="Add Row" onclick="addRow()" />
+                    <br></br>
+                    <button> Submit Data</button>
+                </form>
+            </div>
+        </div>
+        )
+    };
 }
+export default BookingScreen;
