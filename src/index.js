d5ff6178e2345cdf45e69545c7c9b7343a260d51
TeddyShang <tshang3@gatech.edu>
2020-03-30 21:57:58 -0400

    Removed extraneous code

--- src/index.js
+++ src/index.js
@@ -18,28 +18,6 @@ import ProfilePictureUpload from './ProfilePictureUpload';
      ReactDOM.render(<LogInScreen/>, document.getElementById('root'));
 }
 
-/*
-let booking;
-fetch('http://localhost:8080/bookings/5e586b6cb69af82f68c9c6df')
-    .then(res => res.json())
-    .then((function(data){
-        booking = data;
-        console.log(booking);
-        ReactDOM.render(<Upload creatorId = {"5e586a7ab69af82f68c9c6d6"} roomIndex = {0} bookingInformation = {booking}/>, document.getElementById("root"));
-
-    }));
-
-*/
-/*
-let userProfile;
-fetch('http://localhost:8080/userprofiles/5e586a7ab69af82f68c9c6d5')
-.then(res => res.json())
-.then((function(data){
-    userProfile = data;
-    console.log(userProfile);
-    ReactDOM.render(<ProfilePictureUpload/>, document.getElementById("root"));
-}));
-*/
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
